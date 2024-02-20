86e51591d2cffff0e3da238e1ffdf9ade664f9aa
bazunaka <40122285+bazunaka@users.noreply.github.com>
2024-02-20 21:34:30 +0300

    modify navigation

--- bazblog/src/Components/Navigation/Navigation.jsx
+++ bazblog/src/Components/Navigation/Navigation.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { navigationMenu } from './NavigationMenu'
+import { useNavigate } from 'react-router-dom'
 
 const Navigation = () => {
+    const navigate = useNavigate()
     return (
         <div className='h-screen sticky top-0'>
             <div>
@@ -9,7 +11,9 @@ const Navigation = () => {
                     <img src='' alt='Тут картинка' />
                 </div>
                 <div className='space-y-6'>
-                    {navigationMenu.map((item) => <div className='cursor-pointer flex space-x-3'>
+                    {navigationMenu.map((item) => <div className='cursor-pointer flex space-x-3
+                    items-center' onClick={() => item.title==="Profile"?navigate(`/profile/${5}`)
+                    :navigate(item.path)}>
                         {item.icon}
                         <p className='text-xl'>{item.title}</p>
                     </div>)}
