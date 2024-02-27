4b41b059160009725108223deedc6891160efb24
bazunaka <40122285+bazunaka@users.noreply.github.com>
2024-02-27 22:57:19 +0300

    not worK!

--- bazblog/src/Components/Profile/Profile.jsx
+++ bazblog/src/Components/Profile/Profile.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import { useNavigate } from 'react-router-dom';
-import { Avatar, Button } from '@mui/material'
+import { Avatar, Button, Box, Tab, Tabs } from '@mui/material'
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 
 const Profile = () => {
+
+    const [tabValue, setTabValue] = useState("1")
 
     const navigate = useNavigate();
 
@@ -16,6 +24,20 @@ const Profile = () => {
     const handleFollowUser = () => {
         console.log("follow user")
     }
+
+    // export default function LabTabs() {
+    //     const [value, setValue] = React.useState('1');
+
+    const handleTabChange = (event, newValue) => {
+        setValue(newValue);
+
+        if(newValue===4){
+            console.log("likes twit")
+        }
+        else if(newValue===1){
+            console.log("users twits")
+        }
+    };
     return (
         <div>
             <section className={`z-50 flex items-center sticky top-0 bg-opacity-95`}>
@@ -29,17 +51,67 @@ const Profile = () => {
                 <div className='flex justify-between items-start mt-5 h-[5rem]'>
                     <Avatar className='transform -translate-y-24' alt="code with Bazunaka" src=""
                         sx={{ width: "10rem", height: "10rem", border: "4px solid white" }} />
+                    {true ? <Button className='rounded-full' variant='contained'
+                        onClick={handleOpenProfileModel}
+                        sx={{ borderRadius: "20px" }} >
+                        Редактировать профиль
+                    </Button> : <Button className='rounded-full' variant='contained'
+                        onClick={handleFollowUser}
+                        sx={{ borderRadius: "20px" }} >
+                        {true ? "Подписаться" : "Отписаться"}
+                    </Button>}
                 </div>
-                {true ? <Button className='rounded-full' variant='contained'
-                    onClick={handleOpenProfileModel}
-                    sx={{ borderRadius: "20px" }} >
-                    Редактировать профиль
-                </Button> : <Button className='rounded-full' variant='contained'
-                    onClick={handleFollowUser}
-                    sx={{ borderRadius: "20px" }} >
-                    {true ? "Подписаться" : "Отписаться"}
-                </Button>}
-
+                <div>
+                    <div className='flex items-center'>
+                        <h1 className='font-bold text-lg'>Bazunaka</h1>
+                        {true && (<img className='ml-2 w-5 h-5' src={require('./../../img/verify_icon_227246.png')} alt="" />
+                        )}
+                    </div>
+                    <h1 className='text-gray-500'>@Bazunaka</h1>
+                </div>
+                <div className='mt-2 space-y-3'>
+                    <p>Приветствие!</p>
+                    <div className='py-1 flex space-x-5'>
+                        <div className='flex items-center text-gray-500'>
+                            <BusinessCenterIcon />
+                            <p className='ml-2'>Образование</p>
+                        </div>
+                        <div className='flex items-center text-gray-500'>
+                            <LocationOnIcon />
+                            <p className='ml-2'>Россия</p>
+                        </div>
+                        <div className='flex items-center text-gray-500'>
+                            <CalendarMonthIcon />
+                            <p className='ml-2'>Присоединился тогда-то</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center space-x-5'>
+                        <div className='flex items-center space-x-1 font-semibold'>
+                            <span>190</span>
+                            <span className='text-gray-500'>Подписок</span>
+                        </div>
+                        <div className='flex items-center space-x-1 font-semibold'>
+                            <span>590</span>
+                            <span className='text-gray-500'>Подписчиков</span>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <Box sx={{ width: '100%', typography: 'body1' }}>
+                    <TabContext value={tabValue}>
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                            <TabList onChange={handleTabChange} aria-label="lab API tabs example">
+                                <Tab label="Item One" value="1" />
+                                <Tab label="Item Two" value="2" />
+                                <Tab label="Item Three" value="3" />
+                            </TabList>
+                        </Box>
+                        <TabPanel value="1">Item One</TabPanel>
+                        <TabPanel value="2">Item Two</TabPanel>
+                        <TabPanel value="3">Item Three</TabPanel>
+                    </TabContext>
+                </Box>
             </section>
         </div>
     )
