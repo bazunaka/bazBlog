8447291af67a57886e5774b9b9bdb3720a60bfdd
bazunaka-macbook <bazunaka32@gmail.com>
2024-03-01 15:18:09 +0300

    add tweet details

--- bazblog/src/Components/Profile/ProfileModal.jsx
+++ bazblog/src/Components/Profile/ProfileModal.jsx
@@ -70,39 +70,75 @@ export default function ProfileModal() {
               </div>
               <Button type='submit'>Сохранить</Button>
             </div>
-            <div className='overflow-y-scroll overflow-x-hidden h-[80vh]'>
-              <div>
+            <div className='overflow-y-scroll overflow-x-hidden h-[50vh]'>
+              <React.Fragment>
                 <div className='w-full'>
                   <div className='relative'>
                     <img className='w-full h-[12rem] object-cover object-center' src={require('./../../img/0bb51823-25ca-43d6-93f9-23ca920a920c.jpg')} alt="" />
                     <input type="file" className='absolute top-0 left-0 w-full opacity-0 cursor-pointer' name='backgroundImage' onChange={handleImageChange} />
                   </div>
                 </div>
+              </React.Fragment>
+              <div className='space-y-3'>
+                <TextField
+                  fullWidth
+                  id="fullName"
+                  name='fullname'
+                  label="Полное имя"
+                  value={formik.values.fullname}
+                  onChange={formik.handleChange}
+                  error={formik.touched.fullname && Boolean(formik.errors.fullname)}
+                  helperText={formik.touched.fullname && formik.errors.fullname}
+                />
+                <TextField
+                  fullWidth
+                  multiline
+                  rows={4}
+                  id="bio"
+                  name='bio'
+                  label="О себе"
+                  value={formik.values.bio}
+                  onChange={formik.handleChange}
+                  error={formik.touched.bio && Boolean(formik.errors.bio)}
+                  helperText={formik.touched.bio && formik.errors.bio}
+                />
+                <TextField
+                  fullWidth
+                  id="website"
+                  name='website'
+                  label="Веб-сайт"
+                  value={formik.values.website}
+                  onChange={formik.handleChange}
+                  error={formik.touched.website && Boolean(formik.errors.website)}
+                  helperText={formik.touched.website && formik.errors.website}
+                />
+                <TextField
+                  fullWidth
+                  id="location"
+                  name='location'
+                  label="Местоположение"
+                  value={formik.values.location}
+                  onChange={formik.handleChange}
+                  error={formik.touched.location && Boolean(formik.errors.location)}
+                  helperText={formik.touched.location && formik.errors.location}
+                />
+                <div className='my-3'>
+                  <p className='text-lg'>
+                    Дата рождения . Edit
+                  </p>
+                  <p className='text-2xl'>
+                    14 Октября 1993
+                  </p>
+                </div>
+                <p className='py-3 text-lg'>
+                  Редактировать профессиональный профиль
+                </p>
               </div>
             </div>
-            <div className='space-y-3'>
-              <TextField
-                fullWidth
-                id="fullName"
-                name='fullname'
-                label="Полное имя"
-                value={formik.values.fullname}
-                onChange={formik.handleChange}
-                error={formik.touched.fullname && Boolean(formik.errors.fullname)}
-                helperText={formik.touched.fullname && formik.errors.fullname}
-              />
-              <TextField
-                fullWidth
-                multiline
-                rows={4}
-                id="bio"
-                name='bio'
-                label="О себе"
-                value={formik.values.bio}
-                onChange={formik.handleChange}
-                error={formik.touched.bio && Boolean(formik.errors.bio)}
-                helperText={formik.touched.bio && formik.errors.bio}
-              />
+            <div className='w-full transform -translate-y-20 ml-4 h-[6rem]'>
+              <div className='relative'>
+
+              </div>
             </div>
           </form>
         </Box>
