e9b36563a42609b73a56d3cf9632caa2ec5fcd69
bazunaka-macbook <bazunaka32@gmail.com>
2024-03-11 15:30:25 +0300

    add passwordEncoder

--- bazBlog_spring/src/main/java/config/AppConfig.java
+++ bazBlog_spring/src/main/java/config/AppConfig.java
@@ -1,10 +1,13 @@
 package config;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -16,6 +19,7 @@ import java.util.Collections;
 @Configuration
 @EnableWebSecurity
 public class AppConfig {
+    @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
@@ -43,4 +47,8 @@ public class AppConfig {
             }
         };
     }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+}
 }
