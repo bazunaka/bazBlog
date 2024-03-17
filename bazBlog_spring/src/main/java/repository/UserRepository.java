2566e85d44243ff6dba1fa4b975f03075915d435
bazunaka <40122285+bazunaka@users.noreply.github.com>
2024-03-17 23:33:36 +0300

    add search user func

--- bazBlog_spring/src/main/java/repository/UserRepository.java
+++ bazBlog_spring/src/main/java/repository/UserRepository.java
@@ -1,4 +1,17 @@
 package repository;
 
-public class UserRepository {
+import model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query
+    public User findByEmail(String email);
+
+    @Query("SELECT DISTINCT u FROM User u WHERE u.fullName LIKE %:query%")
+    public List<User> searchUser(@Param("query")String query);
 }
