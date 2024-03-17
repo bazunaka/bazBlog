a793fb553fb4f6fd02a14e19f25d4dea7c87b6ad
bazunaka <40122285+bazunaka@users.noreply.github.com>
2024-03-17 23:19:25 +0300

    create UserRepository

--- bazBlog_spring/src/main/java/model/Like.java
+++ bazBlog_spring/src/main/java/model/Like.java
@@ -1,4 +1,19 @@
 package model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Tweet tweet;
 }
